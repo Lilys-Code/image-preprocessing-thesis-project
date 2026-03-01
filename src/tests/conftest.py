5ebe7aaa559a66e6e6218ce66349c304be86b962
Lilys-Code <lilybwatt@outlook.com>
2026-03-01 16:16:57 +1100

    Moved tests around into src

--- src/tests/conftest.py
+++ src/tests/conftest.py
@@ -3,6 +3,6 @@ import sys
 
 # add src folder to python path for tests
 ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir))
-SRC = os.path.join(ROOT, "src")
-if SRC not in sys.path:
-    sys.path.insert(0, SRC)
+# ROOT now equals the 'src' directory; that's what we want on sys.path
+if ROOT not in sys.path:
+    sys.path.insert(0, ROOT)
